f6347047e29ee68b4002bbd1b8a6a189acf415d4
fernandocoylaA <fcoylaa@unsa.edu.pe>
2024-06-03 07:31:51 -0500

    clase por defecto implementada

--- Ejercicio1/compraproductos.java
+++ Ejercicio1/compraproductos.java
@@ -1,1 +1,21 @@
+package compra.productos;
 
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Usuario
+ */
+@WebService(serviceName = "compraproductos")
+public class compraproductos {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }    
+}
